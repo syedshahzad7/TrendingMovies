dcd703ff462b3da0c19e1454f3b16dfb80995718
Syed Shahzad Abdul Wajid <shahzadksa777@gmail.com>
2025-12-23 21:55:43 -0500

    header and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search.jsx'
 
 const App = () => {
+
+  
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <main>
       <div className="pattern"/>
 
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="hero banner"/>
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
 
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
 
 
       </div>
