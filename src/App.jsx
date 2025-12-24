d6aaea136617626c039eca64c2c47833f822bde0
Syed Shahzad Abdul Wajid <shahzadksa777@gmail.com>
2025-12-23 19:19:49 -0500

    Assets downloaded

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <p>Search</p>
+
+
+      </div>
+    </main>
   )
 }
 
