75101668cc32aa96ae2507828ca278e3bb9e3f50
Syed Shahzad Abdul Wajid <shahzadksa777@gmail.com>
2025-12-24 19:51:34 -0500

    Appwrite database integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import MovieCard from './components/MovieCard.jsx';
 import React, { useState, useEffect } from 'react'
 import Search from './components/Search.jsx'
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite.js';
+
 
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -52,6 +54,9 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
       
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
