80ea1a7d4042aee5a38c7df0a08f8f519e646f44
Syed Shahzad Abdul Wajid <shahzadksa777@gmail.com>
2025-12-24 21:03:55 -0500

    Trending movies functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import MovieCard from './components/MovieCard.jsx';
 import React, { useState, useEffect } from 'react'
 import Search from './components/Search.jsx'
 import { useDebounce } from 'react-use';
-import { updateSearchCount } from './appwrite.js';
+import { getTrendingMovies, updateSearchCount } from './appwrite.js';
 
 
 
@@ -24,6 +24,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
@@ -66,10 +67,25 @@ const App = () => {
     }
   }
 
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
+    }
+  }
+
 
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
 
   return (
     <main>
@@ -77,29 +93,42 @@ const App = () => {
 
       <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="hero banner"/>
-          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
 
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
         <section className="all-movies">
-          <h2 className="mt-[40px]">All Movies</h2>
+          <h2>All Movies</h2>
+
           {isLoading ? (
             <Spinner />
-          ):errorMessage ? (
-          <p className='text-red-500'>{errorMessage}</p>
-          ):(
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
             <ul>
               {movieList.map((movie) => (
-                <MovieCard key={movie.id} movie={movie}/>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
-
         </section>
-
       </div>
     </main>
   )
--- src/appwrite.js
+++ src/appwrite.js
@@ -26,7 +26,7 @@ export const updateSearchCount = async (searchTerm, movie) => {
    await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
     count: doc.count + 1,
    })
-  // 3. If it doesn't, create a new document with the search term and count as 1
+  // 3. If it doesn't, create a new document (row) with the search term and count as 1
   } else {
    await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
     searchTerm,
@@ -35,6 +35,20 @@ export const updateSearchCount = async (searchTerm, movie) => {
     poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
    })
   }
+ } catch (error) {
+  console.error(error);
+ }
+}
+
+
+export const getTrendingMovies = async () => {
+ try {
+  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    Query.limit(5),
+    Query.orderDesc("count")
+  ])
+
+  return result.documents;
  } catch (error) {
   console.error(error);
  }
